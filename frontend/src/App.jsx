772bbe432ee103eb324e4842356c48f40fd95624
Albaraae <albaraae.17@gmail.com>
2026-09-24 22:06:04 +0300

    feat(auth): add Signup and Login pages with localStorage and Navbar links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // pages & components
+import Navbar from "./components/Navbar";
 import Home from "./pages/HomePage";
 import AddBookPage from "./pages/AddBookPage";
-import Navbar from "./components/Navbar";
+import BookPage from "./pages/BookPage";
+import EditBookPage from "./pages/EditBookPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import Bookpage from "./pages/BookPage"
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
@@ -15,8 +18,11 @@ const App = () => {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/add-book" element={<AddBookPage />} />
-            <Route path="/books/:id" element={<Bookpage />} />
+            <Route path="/books/:id" element={<BookPage />} />
+            <Route path="/books/add-book" element={<AddBookPage />} />
+            <Route path="/edit-book/:id" element={<EditBookPage />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
@@ -26,4 +32,3 @@ const App = () => {
 };
 
 export default App;
-
